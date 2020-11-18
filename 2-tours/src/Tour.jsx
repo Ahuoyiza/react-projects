6c333f3f657bd375a1fc817448748481ce4759aa
Ahuoyiza <sophieoyiza@gmail.com>
2020-11-18 02:00:26 +0100

    toggle and refresh button function

--- 2-tours/src/Tour.jsx
+++ 2-tours/src/Tour.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-const Tour = ({id, image, info, price, name}) => {
+const Tour = ({id, image, info, price, name, removeTour}) => {
+  const [readMore, setReadMore] = useState(false);
   return <article className="single-tour">
      <img src={image} alt={name}/>
      <footer>
@@ -8,8 +9,14 @@ const Tour = ({id, image, info, price, name}) => {
          <h4>{name}</h4>
          <h4 className="tour-price">${price}</h4>
        </div>
-       <p>{info}</p>
-       <button className="delete-btn">
+       <p>{readMore ? info:`${info.substring(0, 150)}...
+       `}
+      <button onClick={()=> setReadMore(!readMore)}>
+        {
+          readMore ? "Show less" : "Read more"
+        }
+        </button></p>
+       <button className="delete-btn" onClick={()=>removeTour(id)}>
          Not Interested
        </button>
      </footer>
