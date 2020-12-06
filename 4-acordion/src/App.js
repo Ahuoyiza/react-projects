75fd885b402eb59fc4e4f5d0c313480d0fa73705
Ahuoyiza <sophieoyiza@gmail.com>
2020-12-06 22:58:50 +0100

    set up

---
+++ 4-acordion/src/App.js
@@ -0,0 +1,14 @@
+import data from "./data";
+import Question from "./Question"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Acordion</h1>
+      <Question />
+    </div>
+  );
+}
+
+export default App;
